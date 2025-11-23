577decff5707453c9d02b5822f558eacbc8f15a9
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-11-23 19:57:58 +0530

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -5,7 +5,26 @@ fn main() {
     // 3. When the owner goes out of scope, the value will be dropped. 
 
     { // s is not valid here, its not ye declared
-        let s = "hello"; // s is valid from this point forward
-        // do stuff with s 
+        let s1 = String::from("hello"); // s1 is valid from this point forward
+        let s2 = s1.clone();
+        takes_ownership(s2);
+
+        let mut s3 = String::from("hello"); // s is valid from this point forward
+        takes_reference(&s3);
+        takes_mut_reference(&mut s3);
+
+        println!("{}", s3) // we can use s3 here also
     } // this scope is now over, and s is now no longer valid
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn takes_reference(some_string: &String) {
+    println!("{}", some_string);
+}
+
+fn takes_mut_reference(some_string: &mut  String ) {
+    println!("{}", some_string);
 }
