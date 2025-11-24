a60b02f3fd9ae5782643b3ad1b6461a50b27c536
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-11-24 22:09:44 +0530

    enums

---
+++ enums/src/main.rs
@@ -0,0 +1,38 @@
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// Associated function
+impl Message {
+    fn some_function() {
+        println!("Hello from enum fn");
+    }
+}
+
+fn main() {
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    Message::some_function();
+
+    let localhost = IpAddrKind::V4(127, 0, 0, 1);
+
+}
+
+fn route(_ip: IpAddrKind) {
+
+}
