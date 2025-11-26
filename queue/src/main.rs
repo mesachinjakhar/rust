a6b4aaa74b694ba6092538fae0f702dd532ab559
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-11-26 14:19:28 +0530

    queue push_back

--- queue/src/main.rs
+++ queue/src/main.rs
@@ -2,5 +2,11 @@ use std::collections::VecDeque;
 
 fn main() {
     // init empty queue 
-    let queue = VecDeque::new();
+    let mut queue: VecDeque<i32> = VecDeque::new();
+
+    // push back to queue
+    queue.push_back(10);
+    queue.push_back(20);
+    queue.push_back(30);
+    println!("{:?}", queue);
 }
