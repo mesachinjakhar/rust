fd69567762c9b2693fb67526be35e89bed58b64e
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-11-28 21:12:39 +0530

    impl code to store each word count hasmap

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -1,7 +1,58 @@
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::io;
+use std::io::Read;
 
-fn main() {
+
+fn normalize_and_split(text: &str) -> Vec<String> {
+
+    let mut cleaned = String::with_capacity(text.len());
+
+    for ch in text.chars() {
+        if ch.is_alphanumeric() || ch == '\''{
+            cleaned.push(ch)
+    } else {
+        cleaned.push(' ');
+    }
+
+}
+
+cleaned.split_whitespace()
+    .map(|s| s.to_string())
+    .collect()
+}
+
+fn word_freq_map(text: &str) -> HashMap<String, usize> {
+    let mut freq: HashMap<String, usize> = HashMap::new();
+
+    let words = normalize_and_split(text);
+
+    for word in words {
+        *freq.entry(word).or_insert(0) += 1;
+    }
+    freq
+}
+
+
+fn print_sorted(freq: &HashMap<String, usize>) {
+    // We borrow counts & copy because usize is Copy.
+    let mut vec: Vec<(String, usize)> = freq
+        .iter()
+        .map(|(k, &v)| (k.clone(), v))
+        .collect();
+
+
+    vec.sort_by(|a, b| {
+        b.1.cmp(&a.1)
+            .then_with(|| a.0.cmp(&b.0))
+    });
+
+    for (word, count) in vec {
+        println!("{:>6}  {}", count, word);
+    }
+}
+
+fn main() -> io::Result<()> {
     // 1: make empty hashmap 
     let mut map: HashMap <String, usize> = HashMap::new();
     // we make it mut because later we want to insert remove entries
@@ -38,10 +89,20 @@ fn main() {
     struct MyKey {
         a: i32,
         b: i32
-    };
+    }
 
     // Internally HashMap uses a hashing algorithm and a randomized hash state by default (to avoid certain denial-of-service attacks).
     // Average-case HashMap operations (insert, get, remove) are O(1) time.
- 
+
+    // Code to take str as input and create hashmap of each word
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let freq = word_freq_map(&input);
+
+    print_sorted(&freq);
+
+    Ok(())
+
 }
 
