e0a8b53d02b354974562616d0754447806089dbb
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-11-29 12:23:07 +0530

    custom type validation

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,27 @@
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io::Read;
+use std::io;
+
+
+// custom type for validation
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value <1 || value > 100 {
+            panic!("guess value must be between 1 and 100, get: {}.", value)
+        }
+
+        Guess {value: value}
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 
 fn main() {
     // panic stop the code execution immediatley
@@ -28,7 +50,12 @@ fn main() {
     };
 
     // unwrap 
-    let f2 = File::open("hello2.txt").unwrap();
+    // let f2 = File::open("hello2.txt").unwrap();
+    // unwrap will panic if .open method return error
+
+    // expect
+    // let f3 = File::open("hello2.txt").expect("Failed to open hello2.txt");
+    // using expect we pass the custom error message to panic macro
 
 
 }
@@ -46,4 +73,14 @@ fn c(num: i32) {
     if num == 22 {
         panic!("dont pass 22");
     }
+}
+
+// the ? operator
+fn read_username_from_file() -> Result<String, io::Error>{
+    let mut f = File::open("hello.txt")?;
+    // "?" return the error immediately
+
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
