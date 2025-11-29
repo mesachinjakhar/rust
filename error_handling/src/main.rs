a92da17a0efbb351e3e3949e41696e01d1226d37
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-11-29 11:46:59 +0530

    backtracing

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,7 +1,25 @@
 fn main() {
     // panic stop the code execution immediatley
-    panic!("code crashed");
+    // panic!("code crashed");
 
-    println!("this line never prints");
+    // println!("this line never prints");
 
+    // backtrace example
+    a();
+
+}
+
+// backtrace example 
+fn a() {
+    b();
+}
+
+fn b() {
+    c(22)
+}
+
+fn c(num: i32) {
+    if num == 22 {
+        panic!("dont pass 22");
+    }
 }
