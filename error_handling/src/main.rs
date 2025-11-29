3146ea336c923bea57ce6533a78953c6df082bbb
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-11-29 11:53:45 +0530

    result enum

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 fn main() {
     // panic stop the code execution immediatley
     // panic!("code crashed");
@@ -5,7 +7,17 @@ fn main() {
     // println!("this line never prints");
 
     // backtrace example
-    a();
+    // a();
+
+    // run with:  RUST_BACKTRACE=1 cargo run 
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("problem opening the file: {:?}", error),
+    };
+
 
 }
 
