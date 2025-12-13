05c48c76f5cd4a6f97ac33e1645c9a17dcd06d71
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-13 15:12:56 +0530

    linkedlist

---
+++ linkedlist/src/main.rs
@@ -0,0 +1,34 @@
+pub struct Node {
+    val: i32,
+    next: Option<Box<Node>>
+}
+
+impl Node {
+    pub fn new(val: i32) -> Self {
+        Node {
+            val: val,
+            next: None,
+        }
+    }
+}
+
+fn main() {
+    let mut third = Node::new(20);
+
+    let mut second = Node::new(10);
+    second.next = Some(Box::new(third));
+
+    let mut first = Node::new(5); 
+    first.next = Some(Box::new(second));
+
+    let head = Some(Box::new(first));
+    print_list(&head);
+}
+
+
+fn print_list(mut head: &Option<Box<Node>>) {
+    while let Some(node) = head {
+        println!("{}", node.val);
+        head = &node.next;
+    }
+}
