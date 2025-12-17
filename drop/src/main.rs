20cd88298774ec1e7426e8a02a31e5b2a52739a7
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-17 18:40:28 +0530

    drop trait

---
+++ drop/src/main.rs
@@ -0,0 +1,21 @@
+struct CustomSmartPointer {
+    data: String,
+
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointers");
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff")
+    };
+
+    println!("custom smart pointers created");
+}
--- lists/src/first.rs
+++ lists/src/first.rs
@@ -40,3 +40,38 @@ impl List {
         }
     }
 }
+
+#[cfg(test)]
+
+mod test {
+    use crate::first::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(5);
+        list.push(4);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Push some more 
+        list.push(2);
+        list.push(1);
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+    }
+}
