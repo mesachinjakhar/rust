0f4f09325e7d903d082df96a79bb45b4c60ad304
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-18 23:29:04 +0530

    into iter for list

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -1,4 +1,4 @@
-use std::mem;
+use std::{mem, ops::AddAssign};
 
 pub struct List<T> {
     head: Link<T>,
@@ -31,4 +31,39 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()        
+    }
+}
+
+
+#[test]
+fn into_iter() {
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
 }
