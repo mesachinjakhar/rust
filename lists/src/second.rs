7c6e6643b4e53cfc84affeeb0a9c1e146404d84e
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-18 18:34:14 +0530

    using option instead of link enum

---
+++ lists/src/second.rs
@@ -0,0 +1,37 @@
+use std::mem;
+
+pub struct  List {
+    head: Link,
+}
+
+type Link = Option<Box<Node>>;
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List {head: None}
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, None)
+        });
+
+        self.head = Some(new_node)
+    } 
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem);
+            }
+        }
+    }
+}
