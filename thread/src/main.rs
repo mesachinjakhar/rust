28d46f5ac3a6384d84e3d5f2957458d77779b21e
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-20 20:17:54 +0530

    channel

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,18 +1,32 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
+
 
 fn main() {
-    let v = vec![1,2,3];
 
-    let handle = thread::spawn(move || {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        thread::sleep(Duration::from_millis(2000));
+        tx.send(val).unwrap();
+    });
+
+    let recieved = rx.recv().unwrap(); // block the thread until value recieved
+    println!("got: {recieved}");
+
+    // let v = vec![1,2,3];
+
+    // let handle = thread::spawn(move || {
         // for i in 0..10 {
         //     println!("hi number {i}, from the spawned thread!");
         //     thread::sleep(Duration::from_millis(1));
         // }
-        println!("here is a vector: {v:?}");
-    });
+    //     println!("here is a vector: {v:?}");
+    // });
 
-    handle.join().unwrap();
+    // handle.join().unwrap();
 
     // for i in 1..5 {
     //     println!("hi number {i} from the main thread");
