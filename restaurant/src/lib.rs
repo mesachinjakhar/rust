4370cda3c71158e7dc3e795d97d7574c3d1c0862
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-20 13:32:29 +0530

    use keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -36,6 +36,8 @@ mod back_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 
 pub fn eat_at_restaurant() {
     // Order a breakfast in the summer with Rye toast.
@@ -49,5 +51,7 @@ pub fn eat_at_restaurant() {
 
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
 
 }
