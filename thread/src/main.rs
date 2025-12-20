22a7ed94fa1ff31eb39a981cf644f9a6351fb7b4
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-20 19:01:03 +0530

    move keyword

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -2,19 +2,22 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 0..10 {
-            println!("hi number {i}, from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v = vec![1,2,3];
+
+    let handle = thread::spawn(move || {
+        // for i in 0..10 {
+        //     println!("hi number {i}, from the spawned thread!");
+        //     thread::sleep(Duration::from_millis(1));
+        // }
+        println!("here is a vector: {v:?}");
     });
 
     handle.join().unwrap();
-    
-    for i in 1..5 {
-        println!("hi number {i} from the main thread");
-        thread::sleep(Duration::from_millis(1));
-    }
+
+    // for i in 1..5 {
+    //     println!("hi number {i} from the main thread");
+    //     thread::sleep(Duration::from_millis(1));
+    // }
 
     
 }
