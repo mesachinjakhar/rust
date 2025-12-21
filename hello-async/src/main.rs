bead1b70e545c212328ea3daa3d541d36e9d6fa9
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-21 20:56:04 +0530

    spawn task

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,4 +1,5 @@
 use trpl::{Html, Either}; 
+use std::time::Duration;
 
 async fn page_title(url: &str) -> (&str, Option<String>) {
     let response = trpl::get(url).await; 
@@ -11,24 +12,41 @@ async fn page_title(url: &str) -> (&str, Option<String>) {
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    // let args: Vec<String> = std::env::args().collect();
+    // trpl::block_on(async {
+    //     let title_fut_1 = page_title(&args[1]);
+    //     let title_fut_2 = page_title(&args[2]);
+
+
+    //     let (url, maybe_title) = 
+    //     match trpl::select(title_fut_1, title_fut_2).await {
+    //         Either::Left(left) => left,
+    //         Either::Right(right) => right,
+    //     };
+
+    //     println!("{url} returned first");
+    //     match maybe_title {
+    //         Some(title) => println!("its page title was: {title}"),
+    //         None => println!("it had no title"),
+    //     }
+
+    // })
+
     trpl::block_on(async {
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
+        let handle = trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
 
-
-        let (url, maybe_title) = 
-        match trpl::select(title_fut_1, title_fut_2).await {
-            Either::Left(left) => left,
-            Either::Right(right) => right,
-        };
-
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("its page title was: {title}"),
-            None => println!("it had no title"),
+        for i in 1..5 {
+            println!("hi number {i} from the second task");
+            trpl::sleep(Duration::from_millis(500)).await;
         }
 
-    })
+        handle.await.unwrap();
+
+    });
 
 }
