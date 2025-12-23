de578bd0a649986e4346dfa88b739e75025f0771
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-23 22:27:49 +0530

    immutable reference

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -70,7 +70,19 @@ fn main() {
 
     let example_closure = |x| x;
     let s = example_closure(String::from("hello"));
-    let n = example_closure(5);
+    // let n = example_closure(5);
+
+
+    // immutable reference 
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
+
 
 
 
