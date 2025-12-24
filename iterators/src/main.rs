a7e457f13acf12d9711c98936f96025facdf7699
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-24 12:55:37 +0530

    filter adapter

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -2,6 +2,17 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+
 #[test]
     fn iterator_demonstration() {
         let v1 = vec![1, 2, 3];
@@ -24,3 +35,43 @@ fn main() {
 
         assert_eq!(total, 6);
     }
+
+
+    #[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+}
