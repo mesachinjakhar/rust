0c6d5fcb23d95e5a3b3c5b2f7eb64f2308a78395
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-24 20:41:19 +0530

    ..

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -55,10 +55,22 @@ fn main() {
         }
     }
 
-    fn main() {
+  
     let _x = 5;
     let y = 10;
-}
+
+    struct Point {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point { x, .. } => println!("x is {x}"),
+    }
+
 
 
 
