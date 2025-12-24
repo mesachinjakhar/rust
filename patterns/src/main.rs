3fa23491305084178df83eb2901a477189cd4667
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-24 21:10:59 +0530

    @ binding

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -90,7 +90,28 @@ fn main() {
         Some(x) if x %2 == 0 => println!("the number {x} is even"),
         Some(x) => println!("The number {x} is odd"),
         None => (),
+    };
+
+        enum Message {
+        Hello { id: i32 },
     }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {id}"),
+    }
+
+
+
+
+
 
 
 }
