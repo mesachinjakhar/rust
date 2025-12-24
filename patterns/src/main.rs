e875d18b2d6730cb3453b53bda3028f59b804f9f
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-24 20:42:53 +0530

    ..

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -12,40 +12,40 @@ enum Message {
 
 
 fn main() {
-    let p = Point { x: 0, y: 7 };
+    // let p = Point { x: 0, y: 7 };
 
-    let Point { x: a, y: b } = p;
-    assert_eq!(0, a);
-    assert_eq!(7, b);
+    // let Point { x: a, y: b } = p;
+    // assert_eq!(0, a);
+    // assert_eq!(7, b);
 
-    let p = Point { x: 0, y: 7 };
+    // let p = Point { x: 0, y: 7 };
 
-    match p {
-        Point { x, y: 0 } => println!("On the x axis at {x}"),
-        Point { x: 0, y } => println!("On the y axis at {y}"),
-        Point { x, y } => {
-            println!("On neither axis: ({x}, {y})");
-        }
-    }
+    // match p {
+    //     Point { x, y: 0 } => println!("On the x axis at {x}"),
+    //     Point { x: 0, y } => println!("On the y axis at {y}"),
+    //     Point { x, y } => {
+    //         println!("On neither axis: ({x}, {y})");
+    //     }
+    // }
 
-        let msg = Message::ChangeColor(0, 160, 255);
+    //     let msg = Message::ChangeColor(0, 160, 255);
 
-    match msg {
-        Message::Quit => {
-            println!("The Quit variant has no data to destructure.");
-        }
-        Message::Move { x, y } => {
-            println!("Move in the x direction {x} and in the y direction {y}");
-        }
-        Message::Write(text) => {
-            println!("Text message: {text}");
-        }
-        Message::ChangeColor(r, g, b) => {
-            println!("Change color to red {r}, green {g}, and blue {b}");
-        }
-    }
+    // match msg {
+    //     Message::Quit => {
+    //         println!("The Quit variant has no data to destructure.");
+    //     }
+    //     Message::Move { x, y } => {
+    //         println!("Move in the x direction {x} and in the y direction {y}");
+    //     }
+    //     Message::Write(text) => {
+    //         println!("Text message: {text}");
+    //     }
+    //     Message::ChangeColor(r, g, b) => {
+    //         println!("Change color to red {r}, green {g}, and blue {b}");
+    //     }
+    // }
 
-    let ((feet, inches), Point {x,y}) = ((3,10), Point {x:3, y: -10});
+    // let ((feet, inches), Point {x,y}) = ((3,10), Point {x:3, y: -10});
 
     let numbers = (2, 4, 8, 16, 32);
 
