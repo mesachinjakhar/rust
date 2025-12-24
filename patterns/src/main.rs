9c7d32e52ae40752f25cbdf1201878382042b2c9
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-24 20:45:08 +0530

    .. with tuples

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -74,4 +74,13 @@ fn main() {
 
 
 
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+
+
 }
