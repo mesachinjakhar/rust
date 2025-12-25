cf8b16eb1d0f931d457155d7e4cea586b11ae57b
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-25 18:50:41 +0530

    impl with generic

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,36 +1,55 @@
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    // let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    // let result = largest(&number_list);
+    // println!("The largest number is {result}");
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    // let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    // let result = largest(&number_list);
+    // println!("The largest number is {result}");
 
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
+    // let both_integer = Point { x: 5, y: 10 };
+    // let both_float = Point { x: 1.0, y: 4.0 };
+    // let integer_and_float = Point { x: 5, y: 4.0 };
+
+
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
 
 
 }
 
-
-struct Point<T, U> {
+struct Point<T> {
     x: T,
-    y: U,
+    y: T,
 }
 
 
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
     }
-    largest
 }
+
+
+
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+
+
+
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
 
