e3970dd01014f311a27c1bf02f673e9e2486d632
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-25 23:19:34 +0530

    struct lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -4,8 +4,21 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {result}");
+
+        let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
 }
 
 fn longest<'a >(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
 }
+
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
