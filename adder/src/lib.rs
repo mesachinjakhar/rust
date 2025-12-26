889d809202f13bad15226fbf2b44d767f0b01a81
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-26 18:29:42 +0530

    ignore

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -97,6 +97,18 @@ mod tests {
         }
     }
 
+    #[test]
+    #[ignore]
+    fn it_again_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
 
 
 }
