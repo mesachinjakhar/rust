935407c6a92ad2343ef16f37afc2c264016eb1bc
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-26 17:50:24 +0530

    should panic

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,35 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+
+        Guess { value }
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -12,8 +41,43 @@ mod tests {
         assert_eq!(result, 4);
     }
 
+    // #[test]
+    // fn another() {
+    //     panic!("make this test fail");
+    // }
+
     #[test]
-    fn another() {
-        panic!("make this test fail");
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
 }
